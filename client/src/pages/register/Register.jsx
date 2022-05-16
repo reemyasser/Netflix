7159886bc954b2b3df0ba9e5e57ac4c45c9e21a1
Hossam Khaled Mohamed <42701893+Hossam-Khaled-dev@users.noreply.github.com>
2022-05-17 01:45:58 +0200

    Update Register.jsx

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import { useState } from "react";
-import "./register.scss";
+import "./register.css";
 
 export default function Register() {
   const [email, setEmail] = useState("");
