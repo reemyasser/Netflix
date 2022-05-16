40063213ad25e219c77c9c6f23c1ceb9ad82006f
Hossam Khaled Mohamed <42701893+Hossam-Khaled-dev@users.noreply.github.com>
2022-05-16 19:34:22 +0200

    upload code

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./app.scss"
+
+import Register from './pages/register/Register'
+
+import Home from "./pages/home/Home"
+import Watch from './pages/watch/Watch'
+import Login from './pages/login/Login'
+
+
+function App() {
+  return (
+    <div>
+
+ <Login/>
+      
+    </div>
+  )
+}
+
+export default App
+
